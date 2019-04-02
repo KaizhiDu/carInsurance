b6937413a0b573fb80f3dd8d10aaf2d23f00d260
KaizhiDu <529443744@qq.com>
2019-04-02 13:25:51 -0700

    init the project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+mongoose.connect("mongodb+srv://admin-kevin:dkzh19921210@firstdemo-o9czr.mongodb.net/carInsuranceDB", {useNewUrlParser: true});
+
+
+
+app.listen(8080, function() {
+    console.log("Server started on port 8080");
+  });
